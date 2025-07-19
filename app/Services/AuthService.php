d66425ff151aa91dde23a14a932d3a5961f0aceb
alinassarrr <aliinassar8@gmail.com>
2025-07-19 19:28:43 +0300

    Create register service method

--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -5,6 +5,7 @@ namespace App\Services;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthService
 {
@@ -20,6 +21,23 @@ class AuthService
             return null;
         }
         $user =Auth::user();
+        $user->token = $token;
+        return $user;
+    }
+
+    static function register(Request $request){
+        $request->validate([
+            "email"=>["required","string","email"],
+            "password"=>["required","string"],
+            "username"=> ["required","string"]
+        ]);
+        $user = new User; 
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->username = $request->username;
+        $user->save();
+
+        $token = Auth::login($user);
         $user->token = $token;
         return $user;
     }
