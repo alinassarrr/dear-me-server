4a75ca5145195330e31a560060676e1ccea73081
alinassarrr <aliinassar8@gmail.com>
2025-07-19 12:58:30 +0300

    Create CapsuleService for business logic

---
+++ app/Services/CapsuleService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Capsule;
+use DateTime;
+use Illuminate\Http\Request;
+use League\CommonMark\Reference\Reference;
+use function Symfony\Component\Clock\now;
+
+class CapsuleService
+{  
+   
+}
