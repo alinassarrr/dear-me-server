1275fa96493eca5b71d6d992cd9081d562e1728e
alinassarrr <aliinassar8@gmail.com>
2025-07-19 22:43:55 +0300

    Create register validation request

---
+++ app/Http/Requests/RegisterValidationRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterValidationRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email'=>['required','string','email','unique:users'],
+            'password'=>['required','string','min:5'],
+            "username"=> ["required","string"]        
+        ];
+    }
+     public function messages(): array{
+        return [
+            "email.required" => "Email is required",
+            "username.required"=> "Username is required",
+            "email.email"=> "Enter Valid Email",
+            "password.min"=>"Password must be at least 5 characters",
+            'email.unique' => 'This email is already registered. Try logging in instead.',
+
+        ];
+    }
+}
