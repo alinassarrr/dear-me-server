85ec333f637aae8890fe2ece716d867392bd55c6
alinassarrr <aliinassar8@gmail.com>
2025-07-19 22:46:14 +0300

    Change request type and add try catch

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -3,27 +3,30 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginValidationRequest;
+use App\Http\Requests\RegisterValidationRequest;
 use App\Services\AuthService;
-use Illuminate\Http\Request;
 use Exception;
 
 class AuthController extends Controller
 {   
-    public function login(Request $request){
-    $user = AuthService::login($request);
-    if($user){
-        return $this->responseJSON($user);
-    }
-    return $this->responseJSON("Wrong Credentials",401,"failed");
-    }
-
-    public function register(Request $request){
+    public function login(LoginValidationRequest $request){
         try{
-            $user = AuthService::register($request);
+            $user = AuthService::login($request);
             if($user){
                 return $this->responseJSON($user);
             }
-            return $this->responseJSON("Failed to create an account",400,"failed");
+            return $this->responseJSON("Wrong Credentials",401,"failed");
+        }catch(Exception $e){
+            return $this->responseJSON($e->getMessage(),400,"error");
+        }
+
+    }
+    
+    public function register(RegisterValidationRequest $request){
+        try{
+            $user = AuthService::register($request);
+            return $this->responseJSON($user); 
         }
         catch(Exception $e){
             return $this->responseJSON($e->getMessage(),400,"error");
