e8a24219cf85620bd4f229a4a8fe8c1fe6f0bcdc
alinassarrr <aliinassar8@gmail.com>
2025-07-20 02:18:04 +0300

    Implement createCapsule method

--- app/Http/Controllers/Capsule/CapsuleController.php
+++ app/Http/Controllers/Capsule/CapsuleController.php
@@ -20,4 +20,28 @@ class CapsuleController extends Controller
             return $this->responseJSON($e->getMessage(), 500,"failed");
         }
     }
+
+    function createCapsule(Request $request){
+        try{
+            $request->validate([
+            'mood_id' => 'nullable|exists:moods,id',
+            'title' => 'required|string|max:255',
+            'message' => 'required|string',
+            'emoji' => 'required|string',
+            'security' => 'required|in:private,public,unlisted',
+            'tags' => 'required|json    ',
+            'surprise' => 'required|boolean',
+            'reveal_at' => 'required|date|after:now',
+            'image_path' => 'nullable|string',
+            'audio_path' => 'nullable|string',
+            'file_path' => 'nullable|string',
+    ]);
+            $capsule = CapsuleService::createCapsule($request);
+            return $this->responseJSON($capsule);
+        }
+        catch(Exception $e){
+            return $this->responseJSON($e->getMessage(), 500,"failed");
+        }
+    }
+    
 }
