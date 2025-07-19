4916bc4d9244e6f6af458b0cff62e39a38ec02bc
alinassarrr <aliinassar8@gmail.com>
2025-07-19 18:29:50 +0300

    Create route groups with versioning and an auth middleware for authenticated actions

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,25 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\Capsule\CapsuleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post("/all",[CapsuleController::class,"getCapsules"]);
+
+Route::group(["prefix"=>"v0.1"], function(){
+    Route::group(["middleware"=> "auth:api"], function(){
+        Route::group(["prefix"=> "public-wall"], function(){
+            Route::post("/all",[CapsuleController::class,"getCapsules"]); //get capsules dynamically
+        });
+        Route::group(["prefix"=> "profile"], function(){
+            // Route::get("/my-capsules","");
+
+        });
+    });
+    Route::group(["prefix"=> "guest"], function(){
+        Route::post("/login",[AuthController::class,"login"]);
+        // Route::post("/register","");
+    });
+
+});
 
