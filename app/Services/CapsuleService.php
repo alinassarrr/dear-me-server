ea54d74e1056660c9130d6bb0f5644ca7652820f
alinassarrr <aliinassar8@gmail.com>
2025-07-20 16:40:18 +0300

    Add the logic of getting on date and mapping the response back with needed data

--- app/Services/CapsuleService.php
+++ app/Services/CapsuleService.php
@@ -3,7 +3,9 @@
 namespace App\Services;
 
 use App\Models\Capsule;
+use Carbon\Carbon;
 use DateTime;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Http;
@@ -22,19 +24,35 @@ class CapsuleService
    //    return $capules;
    // }
    static function getPublic(Request $request){
-      $logic = Capsule::where("security","public");
+      $logic = Capsule::with(['user:id,username','mood:id,value'])
+      ->where("security","public");
       if($request->filled(['start','end'])){ // make sure on both that they exist first 
          $logic->whereBetween('reveal_at',[$request->start, $request->end]);
       }else{
-         $logic->whereDate("reveal_at", "<=" ,now());
+         $logic->where('reveal_at', '<=', Carbon::now('Asia/Beirut'));
       }
-      $capsules = $logic->orderByDesc('reveal_at')->get();
+      $capsules = $logic->orderByDesc('reveal_at')->get()
+      ->map(function ($capsule){
+         return [
+            'id'=>$capsule->id,
+            'user'=>$capsule->user->username,
+            'mood'=>$capsule->mood->value,
+            'title'=>$capsule->title,
+            'message'=>$capsule->message,
+            'emoji'=>$capsule->emoji,
+            'security'=>$capsule->security,
+            'tags'=>json_decode($capsule->tags),
+            'image_path'=>$capsule->image_path,
+            'audio_path'=>$capsule->audio_path,
+            'file_path'=>$capsule->file_path,
+            'location'=>$capsule->location,
+            'reveal_at'=>$capsule->reveal_at
+         ];
+      });
       return $capsules;
    }
+  
    static function createCapsule(Request $request){
-      $publicIp = Http::get('https://api.ipify.org')->body();
-      $position = Location::get($publicIp)->countryName;
-
       $capsule = new Capsule;
       $capsule->user_id = Auth::id(); 
       $capsule->mood_id = $request->mood_id;
@@ -47,9 +65,16 @@ class CapsuleService
       $capsule->image_path = $request->image_path;
       $capsule->audio_path = $request->audio_path;
       $capsule->file_path = $request->file_path;
-      $capsule->location = $position;
+      $capsule->location = null;
       $capsule->reveal_at = $request->reveal_at;
       $capsule->save();
+
+      // it is taking too much time so we separate it save first with location null and in the background change the location
+      if($publicIp = Http::get('https://api.ipify.org')->body()){
+         $position = Location::get($publicIp)->countryName;
+         $capsule->location = $position;
+         $capsule->save();
+      }
       return $capsule;
    }
    static function getUserCapsules(Request $request){
