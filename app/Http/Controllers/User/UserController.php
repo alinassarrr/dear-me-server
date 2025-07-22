6796b00a539481b6fea9348047ae378130e11dba
alinassarrr <aliinassar8@gmail.com>
2025-07-23 00:01:02 +0300

    Get users data

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    function getUserData(Request $request){
+        $user = UserService::getUserData($request);
+    }
 }
